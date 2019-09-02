bdd5c5237d3fe083d5476d858da99fffe035078a
sami suliman <sami.bco@gmail.com>
2019-09-02 18:51:35 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,5 @@
 class User 
-  @attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name
+  attr_writer :first_name, :last_name
   
-  def first_name=(first_name)
-    first_name = first_name
-  end 
-  
-   def first_name
-    first_name
-  end 
 end 
